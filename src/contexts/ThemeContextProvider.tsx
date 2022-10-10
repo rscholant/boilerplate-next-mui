286a5a217bd6c3793420b8db59a260e26ecc11a1
Rafael Scholant <rafael.scholant@fortbrasil.com.br>
2022-10-10 11:32:54 -0300

    💄 UI: add components and 404 page

--- src/contexts/ThemeContextProvider.tsx
+++ src/contexts/ThemeContextProvider.tsx
@@ -1,8 +1,9 @@
-import { ThemeOptions } from "@mui/material/styles";
-import { ThemeProvider, createTheme } from "@mui/material";
-import { createContext, ReactNode, useContext, useState } from "react";
-import { amber, grey, blueGrey } from "@mui/material/colors";
-import { theme } from "@styles/index";
+import React from 'react';
+import { ThemeOptions } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { amber, grey, blueGrey } from '@mui/material/colors';
+import { theme } from '@styles/index';
 
 type ThemeContextData = {
   switchTheme: () => void;
@@ -12,34 +13,34 @@ export const ThemeContext = createContext({} as ThemeContextData);
 
 const lightThemeOptions: ThemeOptions = {
   palette: {
-    mode: "light",
+    mode: 'light',
     primary: grey,
     divider: grey[900],
     background: {
-      default: "#dadada",
-      paper: "#666666",
+      default: '#dadada',
+      paper: '#666666'
     },
     text: {
-      primary: "#003C3A",
-      secondary: "#292929",
-    },
-  },
+      primary: '#003C3A',
+      secondary: '#292929'
+    }
+  }
 };
 
 const darkThemeOptions: ThemeOptions = {
   palette: {
-    mode: "dark",
+    mode: 'dark',
     primary: blueGrey,
     divider: amber[700],
     background: {
-      default: "#dadada",
-      paper: "#666666",
+      default: '#dadada',
+      paper: '#666666'
     },
     text: {
-      primary: "#fff",
-      secondary: grey[500],
-    },
-  },
+      primary: '#fff',
+      secondary: grey[500]
+    }
+  }
 };
 
 type ThemeContextProviderProps = {
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,2 @@
-export * from "./createEmotionCache";
-export * from "./theme";
+export * from './createEmotionCache';
+export * from './theme';
